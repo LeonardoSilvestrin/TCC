5aba20ba46d78ffdcad24c4b02cd7ddbcee1ba47
Leo <leonardo.silvestrin@usp.br>
2023-02-14 16:47:14 -0300

    1

---
+++ Tentativa 1/meu_rx_nodemcu/meu_rx_nodemcu.ino
@@ -0,0 +1,48 @@
+//Include Libraries
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+//create an RF24 object
+RF24 radio(0, 2);  // CE, CSN
+
+//address through which two modules communicate.
+const byte address[6] = "00001";
+int contador = 0;
+
+struct pacote{
+  int contador;
+  int mensagem = 20;
+  //char mensagem[20];
+};
+
+void setup()
+{
+  Serial.begin(9600);
+  delay(2000);
+
+  radio.begin();
+
+  //set the address
+  radio.openReadingPipe(0, address);
+
+  //Set module as transmitter
+   radio.startListening();
+
+  Serial.println("Esperando Mensagem:");
+  Serial.println(radio.isChipConnected());
+}
+void loop()
+{
+//Read the data if available in buffer
+  if (radio.available())
+  {
+    contador++;
+    pacote mensagemRecebida;
+    radio.read(&mensagemRecebida,sizeof(mensagemRecebida));
+    char buffer[50];
+    sprintf(buffer,"%d - %d",mensagemRecebida.contador,mensagemRecebida.mensagem);
+    Serial.println(mensagemRecebida.contador);
+    Serial.println(mensagemRecebida.mensagem);/*
+  }
+}
---
+++ Tentativa 2/tx_pingpong/tx_pingpong.ino
@@ -0,0 +1,47 @@
+//Include Libraries
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+//create an RF24 object
+RF24 radio(9, 10);  // CE, CSN
+
+//address through which two modules communicate.
+const byte address[6] = "00001";
+int contador = 0;
+
+struct pacote{
+  int contador;
+  int mensagem = 20;
+  //char mensagem[20];
+};
+void setup()
+{
+  Serial.begin(9600);
+  delay(2000);
+
+  radio.begin();
+
+  //set the address
+  //set the address
+  radio.openWritingPipe(address);
+
+  //Set module as receiver
+  radio.stopListening();
+
+  Serial.println("Começando transmissão");
+  Serial.println(radio.isChipConnected());
+}
+
+void loop()
+{
+  contador++;
+  pacote mensagemEnviar;
+  mensagemEnviar.contador = contador;
+  //strcpy(mensagemEnviar.mensagem, " Hello World");
+  radio.write(&mensagemEnviar, sizeof(mensagemEnviar));
+  char buffer[50];
+  sprintf(buffer,"%d - %d",mensagemEnviar.contador,mensagemEnviar.mensagem);
+  Serial.println(buffer);
+  delay(2000);
+}
