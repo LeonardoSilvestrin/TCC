7f112206b1539f19eb7fdf7de218871d26a4efa6
Leo <=>
2023-02-18 16:04:05 -0300

    mensagem 1-1 funcionando via array

--- NRF24L01/meu_rx_nodemcu/meu_rx_nodemcu.ino
+++ NRF24L01/meu_rx_nodemcu/meu_rx_nodemcu.ino
@@ -9,25 +9,17 @@ RF24 radio(0, 2);  // CE, CSN
 //address through which two modules communicate.
 const byte address[6] = "00001";
 int contador = 0;
-
-struct pacote{
-  int contador;
-  char mensagem[20];
-};
-
+float leituras[4];
 void setup()
 {
   Serial.begin(9600);
   delay(2000);
-
   radio.begin();
-
   //set the address
   radio.openReadingPipe(0, address);
 
   //Set module as transmitter
    radio.startListening();
-
   Serial.println("Esperando Mensagem:");
   Serial.println(radio.isChipConnected());
 }
@@ -37,10 +29,20 @@ void loop()
   if (radio.available())
   {
     contador++;
-    pacote mensagemRecebida;
-    radio.read(&mensagemRecebida,sizeof(mensagemRecebida));
-    char buffer[50];
-    sprintf(buffer,"%d - %s",mensagemRecebida.contador,mensagemRecebida.mensagem);
-    Serial.println(buffer);
-    }
+    byte buffer_mensagem[sizeof(leituras)];
+    radio.read(&buffer_mensagem,sizeof(buffer_mensagem));
+    memcpy(&leituras, buffer_mensagem, sizeof(buffer_mensagem));
+    Serial.println(" ------------------------------------- Início da mensagem  -----------------------------------");
+    Serial.print("início da mensagem recebida, id - ");
+    Serial.println(contador);
+    for (int i = 0; i<4; i++){
+      Serial.print("leitura do sensor: ");
+      Serial.print(i+1);
+      Serial.print(": ");
+      Serial.print(leituras[i]);
+      Serial.println("; ");
+    };
+    Serial.println("");
+    Serial.println(" ------------------------------------- fim da transmissão -----------------------------------");
+  }
 }
--- NRF24L01/meu_tx_arduino/meu_tx_arduino.ino
+++ NRF24L01/meu_tx_arduino/meu_tx_arduino.ino
@@ -6,41 +6,44 @@
 //create an RF24 object
 RF24 radio(9, 10);  // CE, CSN
 
-//address through which two modules communicate.
 const byte address[6] = "00001";
 int contador = 0;
+float leituras[4];
 
-struct pacote{
-  int contador;
-  char mensagem[20];
-};
 void setup()
 {
   Serial.begin(9600);
   delay(2000);
-
   radio.begin();
-
-  //set the address
   //set the address
   radio.openWritingPipe(address);
-
   //Set module as receiver
   radio.stopListening();
-
-  Serial.println("Começando transmissão");
+  Serial.print("Começando transmissão - ");
   Serial.println(radio.isChipConnected());
 }
 
 void loop()
 {
   contador++;
-  pacote mensagemEnviar;
-  mensagemEnviar.contador = contador;
-  strcpy(mensagemEnviar.mensagem, " Hello World");
-  radio.write(&mensagemEnviar, sizeof(mensagemEnviar));
-  char buffer[50];
-  sprintf(buffer,"%d - %s",mensagemEnviar.contador,mensagemEnviar.mensagem);
-  Serial.println(buffer);
+  leituras[0]=contador;
+  leituras[1]= contador/2;
+  leituras[2]= contador*contador;
+  leituras[3]= sqrt(contador);
+  byte buffer_mensagem[sizeof(leituras)];
+  memcpy(buffer_mensagem, &leituras, sizeof(leituras));
+  radio.write(&buffer_mensagem, sizeof(buffer_mensagem));
+  Serial.println(" ------------------------------------- Início da transmissão  -------------------------------");
+  Serial.print(" id - ");
+  Serial.println(contador);
+  for (int i = 0; i<4; i++){
+      Serial.print("leitura do sensor ");
+      Serial.print(i+1);
+      Serial.print(": ");
+      Serial.print(leituras[i]);
+      Serial.println("; ");
+   };
+   Serial.println("");
+   Serial.println(" ------------------------------------- fim da transmissão -----------------------------------");
   delay(2000);
 }
